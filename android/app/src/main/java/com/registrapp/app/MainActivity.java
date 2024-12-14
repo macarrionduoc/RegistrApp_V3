5e67d2076741800e1b688d88bb6bd2a47fa41a91
macarrionduoc <ma.carrion@duocuc.cl>
2024-12-14 04:03:39 -0300

    APK se genera apk de la app

---
+++ android/app/src/main/java/com/registrapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.registrapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.registrapp.app',
+  appName: 'registrapp',
+  webDir: 'www'
+};
+
+export default config;
